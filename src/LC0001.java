d564ddeb285d4599aef8e57ae2f0e5fd5feb94fe
bmbewd <bmbewd@gmail.com>
2024-07-23 17:43:55 +0800

    first commit

---
+++ src/LC0001.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class LC0001 {
+    
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        
+        for(int i = 0; i < nums.length; i++){
+            int sub = target - nums[i];
+            if(map.containsKey(sub)){
+                result[0] = map.get(sub);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
